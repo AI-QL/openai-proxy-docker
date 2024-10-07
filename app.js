07dfd469c5b133de346554115cceabdd3fe1ebdd
AiQL.com <ai.infra@outlook.com>
2024-10-07 11:47:08 +0000

    update pkg

--- app.js
+++ app.js
@@ -7,14 +7,22 @@ const target = process.env.TARGET || 'https://api.openai.com'
 app.use('/', createProxyMiddleware({
     target: target,
     changeOrigin: true,
-    onProxyReq: (proxyReq, req, res) => {
-        proxyReq.removeHeader('x-forwarded-for');
-        proxyReq.removeHeader('x-real-ip');
-    },
-    onProxyRes: function (proxyRes, req, res) {
-        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-        proxyRes.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
-    }
+    on: {
+        proxyReq: (proxyReq, req, res) => {
+          /* handle proxyReq */
+          proxyReq.removeHeader('x-forwarded-for');
+          proxyReq.removeHeader('x-real-ip');
+        },
+        proxyRes: (proxyRes, req, res) => {
+          /* handle proxyRes */
+          proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+          proxyRes.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+          console.log(proxyRes)
+        },
+        error: (err, req, res) => {
+          /* handle error */
+        },
+      },
 }));
 
 app.listen(port, () => {
